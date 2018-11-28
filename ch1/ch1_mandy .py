24205837a0cd0082d008673f6a46431a52e91d30
mandycrose <mandycrose@gmail.com>
2018-11-28 16:15:02 +0000

    trying to push

---
+++ ch1/ch1_mandy .py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+4**(1/2)
+2+3
+
+2 + 3
+2.9 + 3.2
+(3.2 / 2.0) + 7
+2**3
+4**(1/2)
+str(6) + "a"
+
+A= 2 + 3
+B= 2.9 + 3.2
+C= (3.2 / 2.0) + 7
+D= 2**3
+E= 4**(1/2)
+F= str(6) + "a"
+
+A = 2 + 3
+B = 2.9 + 3.2
+C = (3.2 / 2.0) + 7
+D = 2**3
+E = 4**(1/2)
+F = str(6) + "a"
+print (A)
+print (B)
+
+print (str("4+5")+' '+'abcd')
+"hello code first".title()
+"hello code first".split()
