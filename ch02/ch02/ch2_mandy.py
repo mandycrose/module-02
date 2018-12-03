aa98bfa0431fd0f075afe1e82f981dcf113a189c
mandycrose <mandycrose@gmail.com>
2018-12-03 16:36:54 +0000

    git push

---
+++ ch02/ch02/ch2_mandy.py
@@ -0,0 +1,113 @@
+## -*- coding: utf-8 -*-
+#"""
+#Created on Thu Nov 29 09:19:57 2018
+#
+#@author: 612436112
+#"""
+#
+#a= 5 - 6
+#b= 8 * 9
+#c= 6 / 2
+#d= 5 / 2
+#e= 5.0 / 2
+#f= 5 % 2
+#g= 2 * (10 + 3)
+#h= 2 ** 4
+#age = 5
+#age = "almost three"
+#age = 29.5
+#age = "I really don’t know!"
+#print (age)
+#
+#print ("hello" + "world")
+#print("\n")
+#print ("Joke " * 3)
+#print ("Chen" + str (3))
+#print ("hello".upper())
+#print ("GOODBYE".lower())
+#print ("the lord of the rings".title())
+#
+#chen = 3 
+#joke = 2
+#print (chen + joke)
+#
+#print ("hello\n"*100)
+#print ("the lord of the rings".title())
+#
+#S1 = "hello" + "world"
+#S2 = "Joke " * 3
+#S3 = 5
+#print(S1+S2*10)
+#print(S1 + S2 + str(S3))
+#
+#
+#strExample= "a-b-c-d-happy" 
+#print (strExample.split("-"))
+#
+#age = 5
+#gender= "boy"
+#holder= "this is a holder"
+#like = "painting"
+#age_description = "My age is {} and I am a {} I like {}.".format(age,gender, like)
+#print (age_description)
+#
+#age = 6
+#gender= "girl"
+#holder= "this is a holder"
+#like = "painting"
+#age_description = "My age is {} and I am a {} I like {}.".format(age,gender, like)
+#print (age_description)
+#
+##this is how you make a comment. 
+#
+#var= """ldjfldkjgd
+#dfljgdlfkjgldkfjgldkjg
+#lsd;flks;dlfks;dlfk
+#d;fkgd;lfkg;dlkg;dflkg"""
+#print (var)
+#
+##chapter 2 homework
+#print(10 / 3)
+#print(10 % 3)
+#
+#a = 1
+#a = a + 1
+#print (a)
+#b = "hello"
+#print (b)
+#c = b.title()
+#print (b)
+#print (c)
+#d = "hello"
+#e = d.title()
+#print (d)
+#print (e)
+#name = "Dave"
+#f = "Hello {0}! ".format(name)
+#print (f)
+#name = "Sarah"
+#print (f)
+#print (f *5)
+#
+#print("I will now count my chickens:")
+#print ("Hens", 25 + 30 / 6)
+#print( "Roosters", 100 - 25 * 3 % 4)
+#print( "Now I will count the eggs")
+#print( 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 )
+#print("Is it true that 3 + 2 < 5 - 7?")
+#print(3 + 2 < 5 - 7)
+
+days= "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan/nFeb/nMar/nApr/nMay/nJun/nJul/nAug"
+print ("Here are the days: ", days)
+
+import ch3_mandy.py
+temperature_conversion(30)
+
+
+
+
+
+
+
+
