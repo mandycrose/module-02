7b0645a7efeb0a2e54d33b2cdb922471eebba3c3
mandycrose <44648092+mandycrose@users.noreply.github.com>
2018-12-19 09:59:24 +0000

    Update ch03_test.py

--- ch03_functions/ch03_test.py
+++ ch03_functions/ch03_test.py
@@ -19,3 +19,17 @@ convert_distance(30)
 cent_to_fahrenheit(0)
 
 #print("this is one function \n" + add_two_numbers(2,2) + "this is another: \n" + convert_distance(50)) 
+
+########### Task 1 #################
+#print ("What’s your name?")
+#name = input()
+#print ("Hello {}!".format(name))
+#
+#name= input("what is your name?      ")
+#location= input ("where are you from?  ")
+#print ("Hello {}!That is cool you are from {}".format(name.upper(),location))
+#
+#name= input("what is your name?      ")
+#input ("where are you from?")
+
+#print ("Hello {}!".format(name.upper()) +)
