2a4803496629b3a555e7636a941d1791ad7480bf
mandycrose <44648092+mandycrose@users.noreply.github.com>
2018-12-19 09:58:49 +0000

    Update ch03_function.py

--- ch03_functions/ch03_function.py
+++ ch03_functions/ch03_function.py
@@ -4,7 +4,6 @@ Created on Mon Dec  3 15:49:19 2018
 
 @author: 612436112
 """
-
 def add_two_numbers(a,b):
     answer= a+b
     print ("{} plus {} is {}".format(a, b, answer))
@@ -14,13 +13,37 @@ def convert_distance(miles):
     print ("Converting distance in miles to kilometers:")
     print ("Distance in miles:", miles)
     print ("Distance in kilometers:", kilometers)
+
     
 def cent_to_fahrenheit(cent):
     fahrenheit= (cent* 9.0) / 5.0 + 32
     print ("Converting centigrade to fahrenheit:")
     print (cent, "degrees centigrade is equal to" , fahrenheit , "degrees farenheit")
 
+
+
 def cent_to_fahrenheit(cent):
     fahrenheit= (cent* 9.0) / 5.0 + 32
     print ("Converting centigrade to fahrenheit:")
     print (str(cent) + " degrees centigrade is equal to " + str(fahrenheit) + " degrees farenheit")
+
+def temperature_conversion(centigrade):
+    fahrenheit=((centigrade*9.0)/5.0)+32
+    kelvin=(centigrade+273.15)
+    return(fahrenheit, kelvin)
+
+############### functions with return ########################################
+    
+def temperature_conversion(centigrade):
+    fahrenheit=((centigrade*9.0)/5.0)+32
+    kelvin=(centigrade+273.15)
+    return(fahrenheit, kelvin)
+
+fahrenheit1, kelvin1 = temperature_conversion(31)
+new_var= fahrenheit1, kelvin1
+print(new_var)
+
+fahrenheit2, kelvin2 = temperature_conversion(45)
+new_var_2= fahrenheit2, kelvin2
+print(new_var_2)
+
