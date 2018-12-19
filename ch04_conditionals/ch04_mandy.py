0ff73d55bfb4f8f51bab03050a37c8667e23e329
mandycrose <44648092+mandycrose@users.noreply.github.com>
2018-12-19 09:51:40 +0000

    Update ch04_mandy.py

--- ch04_conditionals/ch04_mandy.py
+++ ch04_conditionals/ch04_mandy.py
@@ -83,3 +83,76 @@ def ageGame():
     else: 
         print ("you are a pensioner! Lucky you!")
 ageGame()     
+
+
+
+########## shipping weight project #######
+
+#def ground_shipping(weight):
+#  if weight <= 2.0:
+#    return (1.50 * weight)
+#  elif weight >2.0 and weight <= 6.0:
+#    return (3.00 * weight)
+#  elif weight >6.0 and weight <= 10.0:
+#    return (4.0 * weight)
+#  else:
+#    return (weight * 4.75) 
+#
+#print (ground_shipping(11))
+#
+#def ground_shipping(weight):
+#  if weight <= 2.0:
+#    return (4.50 * weight)
+#  elif weight >2.0 and weight <= 6.0:
+#    return (9.00 * weight)
+#  elif weight >6.0 and weight <= 10.0:
+#    return (12.0 * weight)
+#  else:
+#    return (weight * 14.25) 
+
+#def best_shipping(weight):
+#    if weight <= 2.0 and ((1.50 * weight) + 20.00) < ((4.50 * weight) or 125):
+#        print ("your best shipping is ground")
+#    elif weight weight >2.0 and weight <= 6.0 and ((3.00 * weight) + 20.00) < ((9.00 * weight)or 125) :
+#        print("your best shipping is ground")
+        
+#def best_shipping(weight):
+#    if weight <= 2.0 and ((1.50 * weight) + 20) < ((4.50 * weight) or 125):
+#        return ("your best shipping is ground")
+#    elif weight <= 2.0 and ((4.50 * weight) < ((1.50 * weight) + 20) or < 125):
+#        return ("your best shipping is drone")
+#    elif weight <= 2.0 and ((1.50 * weight) + 20) and (4.50 * weight) > 125:
+#        return ("your best shipping is flat rate ground")
+#    else:
+#        print("hold on")
+        
+#def best_shipping(weight):
+#    if weight <= 2.0 and ((1.50 * weight) + 20) < ((4.50 * weight) or 125):
+#        return ("your best shipping is ground")
+#    elif weight <= 2.0 and ((1.50 * weight) + 20) > ((4.50 * weight) or 125):
+#        return ("your best shipping is drone") 
+#    elif weight <= 2.0 and (((4.50 * weight) + 20) or (1.50 * weight)) > 125:
+#        return ("your best shipping is flat rate")
+#    
+#    elif weight >2.0 and weight <= 6.0 and ((3.00 * weight) + 20) < ((9.00 * weight) or 125):
+#        return ("your best shipping is ground")
+#    elif weight >2.0 and weight <= 6.0 and ((3.00 * weight) + 20) > ((9.00 * weight) or 125):
+#        return ("your best shipping is drone") 
+#    elif weight >2.0 and weight <= 6.0 and (((3.00 * weight) + 20) or (9.00 * weight)) > 125:
+#        return ("your best shipping is flat rate")
+#    
+#    elif weight >6.0 and weight <= 10.0 and ((4.00 * weight) + 20) < ((12.00 * weight) or 125):
+#        return ("your best shipping is ground")
+#    elif weight >6.0 and weight <= 10.0 and ((4.00 * weight) + 20) > ((12.00 * weight) or 125):
+#        return ("your best shipping is drone") 
+#    elif weight >6.0 and weight <= 10.0 and (((4.00 * weight) + 20) or (12.00 * weight)) > 125:
+#        return ("your best shipping is flat rate")
+#    
+#    elif weight > 10.0 and  (125 or (14.25 * weight)) > ((4.75 * weight) + 20):
+#        return ("your best shipping is ground")
+#    elif weight > 10.0 and ((4.75 * weight) + 20) > ((14.25 * weight) or 125):
+#        return ("your best shipping is drone") 
+#    elif weight > 10.0 and (125 < (((4.75 * weight) + 20) or (14.25 * weight))):
+#        return ("your best shipping is flat rate")
+#print(best_shipping(400))
+#    
